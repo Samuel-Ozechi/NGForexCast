e67fd1991c24f6cd4c1c42844ecb52a74feb3451
Chiebukar <sam23chuks@gmail.com>
2025-11-23 12:10:48 +0100

    updated fetch_data.py to use SQLAlchemy

--- src/data/fetch_db.py
+++ src/data/fetch_db.py
@@ -1,13 +1,10 @@
 # src/data/fetch_db.py
 
-import psycopg2
 import pandas as pd
-
+from sqlalchemy import create_engine
 from src.config.settings import Settings
 
-# Load global settings instance
 settings = Settings()
-
 
 def fetch_exchange_rates():
     """
@@ -15,7 +12,8 @@ def fetch_exchange_rates():
     and return them as a pandas DataFrame.
     """
     try:
-        conn = psycopg2.connect(settings.SUPABASE_DB_URL, sslmode="require")
+        # Create SQLAlchemy engine using your Postgres URL
+        engine = create_engine(settings.SUPABASE_DB_URL)
 
         query = """
             SELECT date, rate
@@ -23,16 +21,12 @@ def fetch_exchange_rates():
             ORDER BY date ASC;
         """
 
-        df = pd.read_sql_query(query, conn)
+        df = pd.read_sql(query, engine)
         return df
 
     except Exception as e:
         print("❌ Error fetching data:", e)
         return None
-
-    finally:
-        if "conn" in locals():
-            conn.close()
 
 
 if __name__ == "__main__":
