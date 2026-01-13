839685144725f0a753bc0ba49e065e39e894afd6
Chiebukar <sam23chuks@gmail.com>
2026-01-14 00:41:08 +0100

    performance gaurd updated to alignn with drift.py

--- src/monitoring/performance_guard.py
+++ src/monitoring/performance_guard.py
@@ -1,12 +1,12 @@
 # src/monitoring/performance_guard.py
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 from src.monitoring.drift import run_drift_check
 
 
-DRIFT_THRESHOLD = 0.4
-
-def should_retrain(live_df):
-    drift = run_drift_check(live_df)
+def should_retrain():
+    drift = run_drift_check()
 
     if drift["concept_drift"]:
         return True, "concept_drift", drift
@@ -21,28 +21,8 @@ def should_retrain(live_df):
 
 
 if __name__ == "__main__":
-    import pandas as pd
-    import joblib
-    from src.utils.utils import get_prediction_data, get_predictions
-    from src.utils.utils import get_prediction_data
-    from src.config.settings import Settings
 
-    settings = Settings()
-
-    # Fetch recent data for drift check
-    recent_data = get_prediction_data()  
-    print(f"Fetched {recent_data.shape[0]} records for drift check.")
-
-    # Load inference pipeline
-    print("Loading inference pipeline...")
-    pipeline = joblib.load(settings.MODEL_PATH)
-
-    print("Generating live predictions...")
-    # Get live predictions
-    live_df = get_predictions(recent_data, pipeline)
-    print("Live predictions generated:", live_df.shape)
-
-    retrain, reason, drift_report = should_retrain(live_df)
+    retrain, reason, drift_report = should_retrain()
 
     if retrain:
         print(f"Retraining triggered due to: {reason}")
